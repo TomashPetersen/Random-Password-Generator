03364c015561fa0a3cadb88dd1938145e802d915
Denis Volkov <d.g.volkovbz@gmail.com>
2026-05-09 23:33:24 +0400

    Generation and alerting password changed and fixed

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const btnEL = document.querySelector('.btn');
-
 const inputEL = document.getElementById("input");
-
 const copyIconEL = document.querySelector('.fa-copy');
-
 const alertContainerEL = document.querySelector(".alert-container");
+
+let alertTimeout; 
 
 btnEL.addEventListener('click', ()=>{
     createPassword();
@@ -13,8 +12,13 @@ btnEL.addEventListener('click', ()=>{
 copyIconEL.addEventListener('click', ()=>{
     copyPassword();
     if(inputEL.value){
-        alertContainerEL.classList.remove("active")
-        setTimeout(()=>{
+        // 1. Обновляем текст алерта ТОЛЬКО при нажатии на копирование
+        alertContainerEL.innerText = inputEL.value + " Copied!";
+
+        clearTimeout(alertTimeout); 
+        alertContainerEL.classList.remove("active");
+        
+        alertTimeout = setTimeout(()=>{
             alertContainerEL.classList.add("active");
         }, 2000);
     }
@@ -29,13 +33,13 @@ function createPassword() {
         password += chars.substring(randomNum, randomNum + 1);
     }
     inputEL.value = password;
-    alertContainerEL.innerText = password + " Copied!"
+    
+    // 2. УБРАЛИ строчку alertContainerEL.innerText отсюда!
+    // Теперь генерация пароля никак не трогает старый алерт.
 }
 
 function copyPassword() {
     inputEL.select();
     inputEL.setSelectionRange(0, 9999);
     navigator.clipboard.writeText(inputEL.value);
-
 }
-
